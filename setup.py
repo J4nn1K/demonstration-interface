a19090edd1d03a5f0c14a5f0d4a39ecf271bf9d8
J4nn1K <jannik@grothusen.de>
2024-05-16 16:26:35 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='tbd',
+   version='0.0.1',
+   description='tbd',
+   author='Jannik Grothusen',
+   author_email='jannik.grothusen@tuhh.de',
+   packages=find_packages(),
+)
