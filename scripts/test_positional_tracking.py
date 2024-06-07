9db049a7d3f72b346aa79b81f0ca8dadeee6df73
J4nn1K <jannik@grothusen.de>
2024-06-07 17:53:54 +0200

    Update pose transformation

--- scripts/test_positional_tracking.py
+++ scripts/test_positional_tracking.py
@@ -12,32 +12,40 @@ def main():
     cam.enable_tracking()
     print('\n')
     
+    # 135 deg around x-axis
+    R_x = np.array([
+      [1, 0, 0],
+      [0, -np.sqrt(2)/2, -np.sqrt(2)/2],
+      [0, np.sqrt(2)/2, -np.sqrt(2)/2]
+    ])
+    
+    # -90 deg around y-axis
+    R_y = np.array([
+      [0, 0, -1],
+      [0, 1, 0],
+      [1, 0, 0]
+    ])
+    
+    # translation to EE-frame
+    t = np.array([25, 0, -72])
+    
+    R = np.dot(R_y, R_x)
+    
     while True:
       cam.grab_frame()
-      timestamp, confidence, cam_pose = cam.get_pose()
+      _, confidence, cam_pose = cam.get_pose()
       
-      # y-axis rotation by -90 degrees 
-      # ZED overrides 2 of the 3 rotations from initial_world_transform using the IMU gravity
-      rotation_matrix = np.array([
-        [ 0.0,  0.0, -1.0],
-        [ 0.0,  1.0,  0.0], 
-        [ 1.0,  0.0,  0.0],  
-      ])
-     
-      transformation_matrix = np.eye(4)
-      transformation_matrix[:3, :3] = rotation_matrix
+      # transformation into EE-frame
+      T = np.eye(4)
+      T[:3, :3] = R
+      # T[:3, 3] = t
       
-      ee_pose = np.dot(transformation_matrix, cam_pose)
+      ee_pose = np.dot(T, cam_pose)
       
       pos = ee_pose[:3,3]
-      # pos = cam_pose[:3,3]
-      
-      
-      # r = Rotation.from_matrix(ee_pose[:3,:3])
-      # angles = r.as_euler("zyx", degrees=True)
     
       sys.stdout.write('\r')
-      sys.stdout.write(f'x:{pos[0]:5.1f} y:{pos[1]:5.1f} z:{pos[2]:5.1f} - CONFIDENCE: {confidence:2.0f}')
+      sys.stdout.write(f'x:{pos[0]:4.0f} y:{pos[1]:4.0f} z:{pos[2]:4.0f} - CONFIDENCE: {confidence:2.0f}')
       # sys.stdout.write(f'x:{angles[0]:5.1f} y:{angles[1]:5.1f} z:{angles[2]:5.1f} - CONFIDENCE: {confidence:2.0f}')
       sys.stdout.flush()
   
