0836c3910fc28e666ae9e4bd5e687df76fd89229
J4nn1K <jannik@grothusen.de>
2024-06-11 15:19:37 +0200

    Add functionality to switch between cameras

--- web_viewer/app.py
+++ web_viewer/app.py
@@ -1,41 +1,43 @@
 #!/usr/bin/env python
 from flask import Flask, render_template, Response
 import cv2
-import sys
 import numpy as np
 
 from src.components.tracker import Tracker
 from src.components.camera import Camera
 
+
+type = "ZED" 
+
 app = Flask(__name__)
 
-# cam = Tracker()
 
-cam = Camera()
+if type == "RealSense":
+    cam = Camera()
+elif type == "ZED":
+    cam = Tracker()
+else:
+    print("Invalid type")
 
-def get_frame(): # ZED
+
+def get_frame():
     while True:
-        cam.grab_frame()
-        _, image = cam.get_image()
-
-        imgencode=cv2.imencode('.jpg',image)[1]
-        stringData=imgencode.tostring()
-        yield (b'--frame\r\n'
-            b'Content-Type: text/plain\r\n\r\n'+stringData+b'\r\n')
-
-def get_frame(): # RealSense
-    while True:
-        cam.wait_for_frames()
-        image = cam.get_image()  
-        depth = cam.get_depth()
+        if type == "RealSense":
+            cam.wait_for_frames()
+            image = cam.get_image()  
+            depth = cam.get_depth()
         
-        # Convert depth to 8-bit and BGR for OpenCV
-        depth_image_8bit = cv2.normalize(depth, None, 0, 255, cv2.NORM_MINMAX)
-        depth_image_8bit = np.uint8(depth_image_8bit)
-        depth_colormap = cv2.applyColorMap(depth_image_8bit, cv2.COLORMAP_TURBO)
-        depth_bgr = cv2.cvtColor(depth_colormap, cv2.COLOR_RGB2BGR)
+            # Convert depth to 8-bit and BGR for OpenCV
+            depth_image_8bit = cv2.normalize(depth, None, 0, 255, cv2.NORM_MINMAX)
+            depth_image_8bit = np.uint8(depth_image_8bit)
+            depth_colormap = cv2.applyColorMap(depth_image_8bit, cv2.COLORMAP_TURBO)
+            depth_bgr = cv2.cvtColor(depth_colormap, cv2.COLOR_RGB2BGR)
 
-        imgencode=cv2.imencode('.jpg',np.hstack((image, depth_bgr)))[1]
+            imgencode=cv2.imencode('.jpg',np.hstack((image, depth_bgr)))[1]
+        elif type == "ZED":
+            cam.grab_frame()
+            _, image = cam.get_image()
+            imgencode=cv2.imencode('.jpg',image)[1]
         
         stringData=imgencode.tostring()
         yield (b'--frame\r\n'
